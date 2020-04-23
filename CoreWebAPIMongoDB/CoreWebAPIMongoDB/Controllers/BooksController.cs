95828281698915a826411514e386cc06be5de6c2
Rachit Kohli <rachitkohli@gmail.com>
2020-04-23 08:02:29 -0500

    A basic application demonstrating using MongoDB along with .NET Core WebAPI

---
+++ CoreWebAPIMongoDB/CoreWebAPIMongoDB/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWebAPIMongoDB.Model;
+using CoreWebAPIMongoDB.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebAPIMongoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private readonly BookService _bookservice;
+        public BooksController(BookService bookService)
+        {
+            _bookservice = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            return _bookservice.Get();
+        }
+
+        [HttpGet("{category}")]
+        public ActionResult<List<Book>> Get(string category)
+        {
+            return _bookservice.Get(category);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoreWebAPIMongoDB/CoreWebAPIMongoDB/Services/BookService.cs
@@ -0,0 +1,32 @@
+﻿using CoreWebAPIMongoDB.Model;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebAPIMongoDB.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get()
+        {
+            return _books.Find(book => true).ToList();
+        }
+
+        public List<Book> Get(string cat)
+        {
+            return _books.Find(book => book.Category == cat).ToList();
+        }
+    }
+}
